409b4d0e17e3106d6dc01cd73deeb7d90b70e87a
Michael G. Harvey <mharve9@lsu.edu>
2014-04-18 00:00:42 -0500

    color edges based on monophyly

--- R/cloudogram.R
+++ R/cloudogram.R
@@ -4,7 +4,7 @@ getwd()
 ##################################
 ### cloudogram.r ################
 ### By Michael G. Harvey #########
-### 16 April 2014 ################
+### 17 April 2014 ################
 ##################################
 
 library(ape)
@@ -13,7 +13,6 @@ library(plyr)
 ### Read files ###
 
 trees <- read.tree("trees.tre") # read in treefile
-codes <- c(rep(1, 80), rep(2, 20)) # make a vector of some code
 
 par(mfrow=c(1,1), new=F)
 
@@ -22,9 +21,9 @@ par(mfrow=c(1,1), new=F)
 depthlist <- rep(0, 100)
 for (i in 1:length(trees)) {
 	tree <- trees[[i]] # pull out one tree
-	treevector <- data.frame(as.matrix(dist.nodes(tree))[, length(tree$tip)+1], rownames=c(rbind(tree$tip.label), c((length(tree$tip)+1):max(length(dist.nodes(tree)[,1])))))
-	treedepth = max(treevector[,1])
-	depthlist[i] <- treedepth
+	treevector <- data.frame(as.matrix(dist.nodes(tree))[, length(tree$tip)+1], rownames=c(rbind(tree$tip.label), c((length(tree$tip)+1):max(length(dist.nodes(tree)[,1]))))) # vector of node depths
+	treedepth = max(treevector[,1]) # tree depth (deepest node depth)
+	depthlist[i] <- treedepth # add to list
 } 
 maxdepth <- max(depthlist)
 
@@ -32,8 +31,9 @@ maxdepth <- max(depthlist)
 
 for (i in 1:length(trees)) {
 	tree <- trees[[i]] # pull out one tree
-	col <- ifelse(codes[i] == 1, "red", "black") # color based on some criterion
+	code <- ifelse(is.monophyletic(tree, c(1,2)), ifelse(is.monophyletic(tree, c(3,4)), ifelse(is.monophyletic(tree, c(5,6)), ifelse(is.monophyletic(tree, c(7,8)), T, F), F), F), F) # check for monophyly of all 4 species 
+	col <- ifelse(code == T, "black", "red") # color based on some criterion (in this case monophyly of each species)
 	par(mai=c(0.5,0.5,(maxdepth+0.5)-depthlist[i],0.5))
 	plot(tree, type="cladogram", direction = "downwards", y.lim=c(0, depthlist[i]), edge.width=0.5, show.tip.label=F, edge.color=col)
-	par(new=T)
+	par(new=T) # keep using same plot window
 }
